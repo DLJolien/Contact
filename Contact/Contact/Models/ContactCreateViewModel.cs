3f1ff9692b957b5853ecec8e942fac4d080fa535
Jolien De Letter <joliendeletter@gmail.com>
2020-06-09 15:45:17 +0200

    validation added to create contact

--- Contact/Contact/Models/ContactCreateViewModel.cs
+++ Contact/Contact/Models/ContactCreateViewModel.cs
@@ -9,11 +9,23 @@ namespace ContactWeb.Models
     public class ContactCreateViewModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "First name required to fill in.")]
+        [MaxLength(40, ErrorMessage = "Only 40 chars allowed")]
         public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Second name required to fill in.")]
+        [MaxLength(40, ErrorMessage = "Only 40 chars allowed")]
         public string SecondName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email required to fill in.")]
         public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Phone Number required to fill in.")]
         public string PhoneNumber { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Address required to fill in.")]
         public string Address { get; set; }
+
+        [MaxLength(300, ErrorMessage = "Only 300 chars allowed")]
         public string Description { get; set; }
     }
 }
